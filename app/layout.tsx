98c8f03449cf7fd46d1513bbd641625db9f43413
Homayoun <homayoun763@gmail.com>
2024-02-23 23:19:53 +0330

    Ads ads script in head

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,13 @@ export default function RootLayout({
         </Link>
         <p className="text-slate-300">🤟 Welcome to my tech blog. 💻</p>
       </div>
+      <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(e,t,n){e.yektanetAnalyticsObject=n,e[n]=e[n]||function(){e[n].q.push(arguments)},e[n].q=e[n].q||[];var a=t.getElementsByTagName("head")[0],r=new Date,c="https://cdn.yektanet.com/superscript/1tavFZLi/native-mangnify.vercel.app-36551/yn_pub.js?v="+r.getFullYear().toString()+"0"+r.getMonth()+"0"+r.getDate()+"0"+r.getHours(),s=t.createElement("link");s.rel="preload",s.as="script",s.href=c,a.appendChild(s);var l=t.createElement("script");l.async=!0,l.src=c,a.appendChild(l)}(window,document,"yektanet");
+            `,
+          }}
+        />
     </header>
   );
 
@@ -34,7 +41,7 @@ export default function RootLayout({
   );
 
   return (
-    <html>
+    <html dir="rtl" lang="fa">
       <head />
       <body>
         <div className="mx-auto  max-w-2xl px-6">
