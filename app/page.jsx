63a18810d7d181c6ba5662c85f84dae7ec94f21b
Homayoun <homayoun763@gmail.com>
2024-03-12 23:26:37 +0330

    deploy the home page with data

--- app/page.jsx
+++ app/page.jsx
@@ -3,8 +3,7 @@ import Dashboard from "./ui/home/Dashboard"
 const Home = () => {
   return (
     <div>
-      {/* <Dashboard /> */}
-      Test
+      <Dashboard />
       <div id="pos-article-display-94409"></div>
       <div id="pos-article-text-94402"></div>
     </div>
