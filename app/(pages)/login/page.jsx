a53533559194e0628bd212dff7941befe499d54e
Homayoun <homayoun763@gmail.com>
2024-03-19 02:20:08 +0330

    simple login to admin

---
+++ app/(pages)/login/page.jsx
@@ -0,0 +1,38 @@
+'use client'
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
+
+  const handleLogin = () => {
+    if (username == "test" && password == "123456") {
+      router.push('/admin');
+    } else {
+      alert('Invalid username or password');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login Page</h2>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default Login;
