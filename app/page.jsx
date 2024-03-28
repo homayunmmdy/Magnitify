dcc14619be6fc6d30596bbd3b6ec1ba898ccb3fd
Homayoun <homayoun763@gmail.com>
2024-03-28 23:29:13 +0330

    fix rows for ads

--- app/page.jsx
+++ app/page.jsx
@@ -17,30 +17,30 @@ const Home = () => {
             <NextMainSec />
           </div>
         </div>
-        <div className="col-span-12 sm:col-span-6 lg:col-span-0 xl:col-span-2">
-      <div id="pos-article-display-94687"></div>
+        <div className="col-span-0 hidden sm:grid sm:col-span-6 lg:col-span-0 xl:col-span-2">
+          <div id="pos-article-display-94687"></div>
         </div>
       </div>
       <div className="grid grid-cols-12 px-5 md:px-10 gap-2">
-      <div className="col-span-0 md:col-span-2">
-        <div id="pos-article-display-94698"></div>
-      </div>
+        <div className="col-span-0 hidden md:grid md:col-span-2">
+          <div id="pos-article-display-94698"></div>
+        </div>
 
-      <div className="col-span-12 md:col-span-8">
-      <PostSec title="هوش مصنوعی" place="ai" />
-      <div id="pos-article-display-94981"></div>
-      <PostSec title="برنامه نویسی" place="programming" />
-        <div id="pos-article-text-94402"></div>
-      <PostSec title="اندرو تیت" place="andrew tate" />
-        <div id="pos-article-display-94409"></div>
-      <PostSec title="کتاب" place="books" />
-      <div id="pos-article-display-94980"></div>
-      </div>
+        <div className="col-span-12 md:col-span-8">
+          <PostSec title="هوش مصنوعی" place="ai" />
+          <div id="pos-article-display-94981"></div>
+          <PostSec title="برنامه نویسی" place="programming" />
+          <div id="pos-article-text-94402"></div>
+          <PostSec title="اندرو تیت" place="andrew tate" />
+          <div id="pos-article-display-94409"></div>
+          <PostSec title="کتاب" place="books" />
+          <div id="pos-article-display-94980"></div>
+        </div>
 
-      <div className="col-span-0 md:col-span-2">
-        <div id="pos-article-display-94697"></div>
+        <div className="col-span-0 hidden md:grid md:col-span-2">
+          <div id="pos-article-display-94697"></div>
+        </div>
       </div>
-    </div>
       <Newsletter />
     </div>
   );
