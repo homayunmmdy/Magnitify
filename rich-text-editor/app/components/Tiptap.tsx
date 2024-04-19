7d9a9ec5fe313c7ba1c746845acb0415a9181f88
Homayoun <homayoun763@gmail.com>
2024-04-19 19:40:58 +0330

    doc and name list

--- rich-text-editor/app/components/Tiptap.tsx
+++ rich-text-editor/app/components/Tiptap.tsx
@@ -1,14 +1,17 @@
 "use client";
-import "./style.css"
+import "./style.css";
 import Document from "@tiptap/extension-document";
 import Paragraph from "@tiptap/extension-paragraph";
 import Underline from "@tiptap/extension-underline";
 import Text from "@tiptap/extension-text";
-import Heading from '@tiptap/extension-heading'
-import Highlight from '@tiptap/extension-highlight'
-import Bold from '@tiptap/extension-bold'
-import Image from '@tiptap/extension-image'
-import Blockquote from '@tiptap/extension-blockquote'
+import Heading from "@tiptap/extension-heading";
+import Highlight from "@tiptap/extension-highlight";
+import Bold from "@tiptap/extension-bold";
+import Image from "@tiptap/extension-image";
+import Blockquote from "@tiptap/extension-blockquote";
+import ListItem from "@tiptap/extension-list-item";
+import BulletList from "@tiptap/extension-bullet-list";
+import OrderedList from "@tiptap/extension-ordered-list";
 import { useEditor, EditorContent } from "@tiptap/react";
 import { LuUnderline } from "react-icons/lu";
 import { RiMarkPenLine } from "react-icons/ri";
@@ -16,6 +19,8 @@ import { useCallback } from "react";
 import { BiSolidImageAdd } from "react-icons/bi";
 import { BsTypeBold } from "react-icons/bs";
 import { FaQuoteRight } from "react-icons/fa6";
+import { PiListBulletsFill } from "react-icons/pi";
+import { PiListNumbersFill } from "react-icons/pi";
 
 const Tiptap = () => {
   const editor = useEditor({
@@ -26,6 +31,9 @@ const Tiptap = () => {
       Text,
       Image,
       Underline,
+      BulletList,
+      OrderedList,
+      ListItem,
       Blockquote,
       Highlight.configure({ multicolor: true }),
       Heading.configure({
@@ -38,12 +46,12 @@ const Tiptap = () => {
   });
 
   const addImage = useCallback(() => {
-    const url = window.prompt('URL')
+    const url = window.prompt("URL");
 
     if (url) {
-      editor.chain().focus().setImage({ src: url }).run()
+      editor.chain().focus().setImage({ src: url }).run();
     }
-  }, [editor])
+  }, [editor]);
 
   if (!editor) {
     return null;
@@ -52,18 +60,18 @@ const Tiptap = () => {
   return (
     <>
       <div className="mb-2  flex items-center gap-4">
-      <button
-        onClick={() => editor.chain().focus().toggleBold().run()}
-        className={`btn ${editor.isActive('bold') ? 'is-active' : ''}`}
-      >
-        <BsTypeBold />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleBlockquote().run()}
-        className={`btn ${editor.isActive('blockquote') ? 'is-active' : ''}`}
-      >
-        <FaQuoteRight />
-      </button>
+        <button
+          onClick={() => editor.chain().focus().toggleBold().run()}
+          className={`btn ${editor.isActive("bold") ? "is-active" : ""}`}
+        >
+          <BsTypeBold />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleBlockquote().run()}
+          className={`btn ${editor.isActive("blockquote") ? "is-active" : ""}`}
+        >
+          <FaQuoteRight />
+        </button>
         <button
           onClick={() => editor.chain().focus().toggleUnderline().run()}
           className={`btn ${editor.isActive("underline") ? "is-active" : ""}`}
@@ -71,31 +79,57 @@ const Tiptap = () => {
           <LuUnderline />
         </button>
         <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
-        className={`btn ${editor.isActive('heading', { level: 1 }) ? 'is-active' : ''}`}
-        title="Control + Alt + 1"
-      >
-        H1
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
-        className={`btn ${editor.isActive('heading', { level: 2 }) ? 'is-active' : ''}`}
-      >
-        H2
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
-        className={`btn ${editor.isActive('heading', { level: 3 }) ? 'is-active' : ''}`}
-      >
-        H3
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHighlight().run()}
-        className={`btn ${editor.isActive('highlight') ? 'is-active' : ''}`}
-      >
-        <RiMarkPenLine />
-      </button>
-      <button className="btn" onClick={addImage}><BiSolidImageAdd /></button>
+          onClick={() => editor.chain().focus().toggleOrderedList().run()}
+          className={`btn ${editor.isActive("orderedList") ? "is-active" : ""}`}
+        >
+          <PiListNumbersFill />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleBulletList().run()}
+          className={`btn ${editor.isActive("bulletList") ? "is-active" : ""}`}
+        >
+          <PiListBulletsFill />
+        </button>
+        <button
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 1 }).run()
+          }
+          className={`btn ${
+            editor.isActive("heading", { level: 1 }) ? "is-active" : ""
+          }`}
+          title="Control + Alt + 1"
+        >
+          H1
+        </button>
+        <button
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 2 }).run()
+          }
+          className={`btn ${
+            editor.isActive("heading", { level: 2 }) ? "is-active" : ""
+          }`}
+        >
+          H2
+        </button>
+        <button
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 3 }).run()
+          }
+          className={`btn ${
+            editor.isActive("heading", { level: 3 }) ? "is-active" : ""
+          }`}
+        >
+          H3
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleHighlight().run()}
+          className={`btn ${editor.isActive("highlight") ? "is-active" : ""}`}
+        >
+          <RiMarkPenLine />
+        </button>
+        <button className="btn" onClick={addImage}>
+          <BiSolidImageAdd />
+        </button>
       </div>
       <div className=" p-2">
         <EditorContent editor={editor} />
