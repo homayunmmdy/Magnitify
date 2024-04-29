2cef554b7c592064035a8e8b4358c1f0ed644d67
Homayoun <homayoun763@gmail.com>
2024-04-30 01:51:40 +0330

    hero section order in the mobile

--- app/[locale]/components/Main.jsx
+++ app/[locale]/components/Main.jsx
@@ -1,20 +1,19 @@
 import React from 'react'
 
-const Main = ({post}) => {
+const Main = ({ post }) => {
     return (
         <>
             <a href={`/Posts/${post._id}`} className='grid grid-cols-12  gap-4 md:gap-8'>
-
-                <div className='col-span-12 md:col-span-6 xl:col-span-5'>
+                <div className='col-span-12 xl:col-span-5'>
                     <img src={post.imgurl}
-                            alt={post.title}
-                            className="w-full h-[450px] object-cover rounded-md"
-                        />
+                        alt={post.title}
+                        className="w-full h-full sm:h-[450px] object-cover rounded-md"
+                    />
                 </div>
-                <div className='col-span-12 md:col-span-6 xl:col-span-7 grid items-center'>
+                <div className='col-span-12 xl:col-span-7 grid items-center'>
                     <section className='py-4'>
-                        <h1 className="text-5xl font-light text-black mb-6 sm:text-6xl md:text-7xl tracking-wide racking-wide leading-loose">
-                           {post.title}
+                        <h1 className="text-4xl font-light text-black mb-6 sm:text-6xl xl:text-6xl tracking-wide leading-relaxed">
+                            {post.title}
                         </h1>
                         <p className='leading-10 font-medium'>
                             {post.description}
--- app/[locale]/components/NextMain.jsx
+++ app/[locale]/components/NextMain.jsx
@@ -4,11 +4,7 @@ const NextMain = ({ post }) => {
     return (
         <>
             <a href={`/Posts/${post._id}`} className='grid grid-cols-12  gap-4 md:gap-8 items-center mb-4'>
-                <div className='col-span-12 md:col-span-6 xl:col-span-8'>
-                    <p className='font-light my-2 text-lg leading-relaxed'>
-                        {post.title}
-                    </p>
-                </div>
+               
                 <div className='col-span-12 md:col-span-6 xl:col-span-4'>
                     <div>
                         <img src={post.imgurl}
@@ -16,6 +12,11 @@ const NextMain = ({ post }) => {
                             className="w-full h-full aspect-video object-cover rounded-md"
                         />
                     </div>
+                </div>
+                <div className='col-span-12 md:col-span-6 xl:col-span-8'>
+                    <p className='font-light my-2 text-lg leading-relaxed'>
+                        {post.title}
+                    </p>
                 </div>
             </a>
         </>
