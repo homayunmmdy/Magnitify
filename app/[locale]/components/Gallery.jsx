891ffa2e1517160a3c05a4a55ba4b2bb2f6b3606
Homayoun <homayoun763@gmail.com>
2024-04-30 01:36:04 +0330

    Gallery section

--- app/[locale]/components/Gallery.jsx
+++ app/[locale]/components/Gallery.jsx
@@ -6,49 +6,58 @@ const Gallery = () => {
             <div className=" mx-auto  flex items-center">
                 <div className="-m-1 flex flex-row sm:flex-wrap md:-m-2">
                     <div className="flex w-full sm:w-1/2 flex-wrap lg:flex-row flex-row-reverse">
-                        <div className="w-full lg:w-1/2 p-1 md:p-2">
+                        <div className="relative overflow-hidden w-full lg:w-1/2 p-1 md:p-2">
                             <img
-                                alt="gallery"
+                                alt="کتاب"
                                 className="block h-full w-full rounded-lg 2xl:rounded-2xl object-cover object-center"
-                                src="https://www.tailwindtap.com/assets/components/gallery/image1.jpg"
+                                src="/1.jpg"
                             />
+                            <h3 className='z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-3xl'>کتاب</h3>
                         </div>
-                        <div className="w-full lg:w-1/2 p-1 md:p-2">
+
+                        <div className="relative overflow-hidden w-full lg:w-1/2 p-1 md:p-2">
                             <img
-                                alt="gallery"
+                                alt="بیوگرافی"
                                 className="block h-full w-full rounded-lg 2xl:rounded-2xl object-cover object-center"
-                                src="https://www.tailwindtap.com/assets/components/gallery/image2.jpg"
+                                src="/2.jpg"
                             />
+                            <h3 className='z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-3xl'>بیوگرافی</h3>
+
                         </div>
-                        <div className="w-full p-1 md:p-2">
+                        <div className="relative overflow-hidden w-full p-1 md:p-2">
                             <img
-                                alt="gallery"
+                                alt="برنامه نویسی"
                                 className="block h-full w-full rounded-lg 2xl:rounded-2xl object-cover object-center max-h-none lg:max-h-[1000px]"
-                                src="https://www.tailwindtap.com/assets/components/gallery/image7.jpg"
+                                src="/3.jpg"
                             />
+                            <h3 className='z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-3xl'>برنامه نویسی</h3>
                         </div>
                     </div>
-                    <div className="flex w-full sm:w-1/2 flex-wrap">
+                    <div className="relative overflow-hidden flex w-full sm:w-1/2 flex-wrap">
                         <div className="w-full p-1 md:p-2">
                             <img
-                                alt="gallery"
+                                alt="پول و کسب و کار"
                                 className="block h-full w-full rounded-lg 2xl:rounded-2xl object-cover object-center"
-                                src="https://www.tailwindtap.com/assets/components/gallery/image9.jpg"
+                                src="/4.jpg"
                             />
+                            <h3 className='z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-3xl'> پول و کسب و کار</h3>
                         </div>
-                        <div className="w-1/2 p-1 md:p-2">
+                        <div className="relative overflow-hidden w-1/2 p-1 md:p-2">
                             <img
-                                alt="gallery"
+                                alt="بهبود خود"
                                 className="block h-full w-full rounded-lg 2xl:rounded-2xl object-cover object-center"
-                                src="https://www.tailwindtap.com/assets/components/gallery/image4.jpg"
+                                src="/5.jpg"
                             />
+                            <h3 className='z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-3xl'>بهبود خود</h3>
                         </div>
-                        <div className="w-1/2 p-1 md:p-2">
+                        <div className="relative overflow-hidden w-1/2 p-1 md:p-2">
                             <img
-                                alt="gallery"
+                                alt="هوش مصنوعی"
                                 className="block h-full w-full rounded-lg 2xl:rounded-2xl object-cover object-center"
-                                src="https://www.tailwindtap.com/assets/components/gallery/image6.jpg"
+                                src="/6.jpg"
                             />
+                            <h3 className='z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-3xl'>هوش مصنوعی</h3>
+
                         </div>
                     </div>
                 </div>
