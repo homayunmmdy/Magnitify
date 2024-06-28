2ed61dd1fa90012b17dcdd2c2c64126f6a4225c1
Homayoun <homayoun763@gmail.com>
2024-06-28 15:58:12 +0330

    - Publication Skeleton - Remove Swiper - Gallery Effect

--- app/components/sections/publications/PublicationCard.jsx
+++ app/components/sections/publications/PublicationCard.jsx
@@ -4,16 +4,16 @@ import React from 'react'
 const PublicationCard = ({ publication }) => {
     return (
         <>
-            <div className="card bg-base-100 w-96 shadow-xl">
+            <div className="group card bg-base-100 border-2 border-slate-100 hover:border-indigo-700 delay-100 ease-in-out duration-300 shadow-xl" key={publication._id}>
                 <Link href={`/publication/${publication._id}`} className="px-10 pt-10">
                     <img
                         src={publication.imgurl}
                         title={publication.title}
                         alt={publication.title}
-                        className="rounded-xl" />
+                        className="rounded-xl transition duration-300 delay-100 group-hover:scale-105" />
                 </Link>
                 <div className="card-body items-center text-center">
-                    <h2 className="card-title">{publication.title}</h2>
+                    <h2 className="card-title transition duration-300 delay-100 group-hover:text-indigo-700">{publication.title}</h2>
                     <p>{publication.description}</p>
                 </div>
             </div>
--- app/components/sections/publications/Publications.jsx
+++ app/components/sections/publications/Publications.jsx
@@ -2,35 +2,21 @@
 import useFetch from '@/app/hooks/useFetch'
 import React from 'react'
 import PublicationCard from './PublicationCard'
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, EffectFade, Autoplay } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/effect-fade";
-import "swiper/css/autoplay";
+import PublicationsSkeleton from './PublicationsSkeleton'
 
 const publications = () => {
-    const { data  } = useFetch('/api/publications');
-    const publicationsData = data.slice(8)
+    const { data , loading } = useFetch('/api/publications');
+    const publicationsData = data?.slice(-4)
+    if(loading) {
+        return <PublicationsSkeleton />
+    }
     return (
         <>
             <div className=" mx-auto py-6">
                 <div className="grid grid-cols-1 md:grid-cols-4 sm:grid-cols-2 gap-5">
-                    <Swiper
-                        modules={[Navigation, EffectFade, Autoplay]}
-                        effect="fade"
-                        spaceBetween={5}
-                        slidesPerView={4}
-                        navigation
-                        loop={true}
-                        autoplay={{ delay: 4000 }}
-                    >
-                        {publicationsData?.map(publication => (
-                              <SwiperSlide key={publication._id}>
-                                  <PublicationCard publication={publication} />
-                              </SwiperSlide>
-                        ))}
-                    </Swiper>
+                    {publicationsData?.map(publication => (
+                        <PublicationCard publication={publication} />
+                    ))}
                 </div>
             </div>
 
