2037164bb3e85698dface13b7950e6183bc9513d
Homayoun <129702378+homayunmmdy@users.noreply.github.com>
2024-07-20 10:28:24 +0330

    Update page.jsx

--- app/(pages)/Posts/[id]/page.jsx
+++ app/(pages)/Posts/[id]/page.jsx
@@ -10,6 +10,7 @@ import { FaPlay } from "react-icons/fa";
 import { FaStop } from "react-icons/fa6";
 import { readingTime } from 'reading-time-estimator';
 import PostSeclton from "./PostSkelton";
+import { FaRegMessage } from "react-icons/fa6";
 
 const Post = () => {
   const post = useSinglePost();
@@ -85,9 +86,8 @@ const Post = () => {
     <div class="relative group">
       <div class="absolute -inset-1 bg-gradient-to-r from-purple-600 to-pink-600 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
       <div class="relative px-7 py-6 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-top justify-start space-x-6">
-        <svg class="w-8 h-8 text-purple-600" fill="none" viewBox="0 0 24 24">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M6.75 6.75C6.75 5.64543 7.64543 4.75 8.75 4.75H15.25C16.3546 4.75 17.25 5.64543 17.25 6.75V19.25L12 14.75L6.75 19.25V6.75Z"></path>
-        </svg>
+        <FaRegMessage size={24}/>
+
         <div class="space-y-2">
           <p class="text-slate-800">Hi, my name is Homayoun. I am the founder of Magnitify. At Magnitify, we write articles about AI, business, and self-improvement. We share insights from books, document our experiences, and explore what we learn about these three topics. I hope you enjoy our content!</p>
         </div>
