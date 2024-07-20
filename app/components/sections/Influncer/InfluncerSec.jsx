0e1d12a9eb9c1f660913c5878a8f96551a3ce9d1
Homayoun <129702378+homayunmmdy@users.noreply.github.com>
2024-07-20 11:27:51 +0330

    Update InfluncerSec.jsx

--- app/components/sections/Influncer/InfluncerSec.jsx
+++ app/components/sections/Influncer/InfluncerSec.jsx
@@ -15,15 +15,13 @@ const InfluncerSec = () => {
   return (
     <>
       {data?.map((post, _index) => (
-        <Link href={`/Posts/${post._id}`} className="shadow-xl flex flex-col justify-center col-span-12 align-middle bg-no-repeat bg-cover lg:col-span-6 lg:h-auto"
+        <Link href={`/Posts/${post._id}`} className="shadow-xl flex flex-col justify-end col-span-12 align-middle bg-no-repeat bg-cover lg:col-span-6 lg:h-auto"
           style={{ backgroundImage: `url(${post.imgurl})` }}
           key={post._id}
           title={post.title}
         >
-          <div className="flex flex-col items-center p-8 py-12 text-center">
-            <h3 className="py-4 text-5xl font-bold">{post.title}</h3>
-            <p className="pb-6">{post.description}</p>
-            <GrLinkNext className="animate-bounce" size={28} />
+          <div className="flex flex-col items-center p-4 text-center bg-[rgba(111,111,111,.6)] text-white">
+            <h3 className="text-3xl font-bold">{post.title.slice(0,100)}</h3>
           </div>
         </Link>
       ))}
