3448ecb564affa19699fe03aae2dad571b172781
Homayoun <129702378+homayunmmdy@users.noreply.github.com>
2024-07-20 11:00:45 +0330

    Update NextMain.jsx

--- app/components/sections/Hero/NextMain.jsx
+++ app/components/sections/Hero/NextMain.jsx
@@ -21,7 +21,7 @@ const NextMain = ({ post }) => {
                 </div>
                 <div className='col-span-12 md:col-span-6 xl:col-span-8'>
                     <h2 className='font-light my-2 text-lg leading-relaxed'>
-                        {post.title.slice(0,120)}
+                        {post.title.slice(0,75)}...
                     </h2>
                 </div>
             </Link>
