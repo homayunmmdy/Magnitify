db4a71effdd4060fc07dcce1a85a7f6328fe05b8
Homayoun <homayoun763@gmail.com>
2024-10-25 21:07:56 +0330

    fix : post amount

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ const DemoPage = () => {
         <MainSec />
         <div className="grid grid-cols-12 md:gap-9">
           <div className="col-span-12 md:col-span-9">
-            <PostsSec amount={3} id={2} title="هوش مصنوعی" />
+            <PostsSec amount={-3} id={2} title="هوش مصنوعی" />
           </div>
           <div className="col-span-12 md:col-span-3">
             <LinearAds />
@@ -17,21 +17,21 @@ const DemoPage = () => {
       </div>
       <div className="bg-indigo-600 p-3 sm:p-4 md:p-6 lg:p-9">
         <div className="w-[94%] md:w-[92%] mx-auto mt-5'">
-          <PostsSec amount={4} id={3} title="تجربه من" />
+          <PostsSec amount={-4} id={3} title="تجربه من" />
         </div>
       </div>
       <div className="w-[94%] md:w-[92%] mx-auto pt-2">
-        <PostsSec amount={4} id={4} title="کسب و کار" />
+        <PostsSec amount={-4} id={4} title="کسب و کار" />
       </div>
       <div className="bg-indigo-600 p-3 sm:p-4 md:p-6 lg:p-9">
         <div className="w-[94%] md:w-[92%] mx-auto mt-5'">
-          <PostsSec amount={4} id={5} title="بیوگرافی" />
+          <PostsSec amount={-4} id={5} title="بیوگرافی" />
         </div>
       </div>
       <div className="w-[94%] md:w-[92%] mx-auto pt-2">
         <div className="grid grid-cols-12 md:gap-9">
           <div className="col-span-12 md:col-span-9">
-            <PostsSec amount={3} id={6} title="پست های بیشتر" />
+            <PostsSec amount={-3} id={6} title="پست های بیشتر" />
           </div>
           <div className="col-span-12 md:col-span-3">
             <div id="pos-article-display-101799"></div>
