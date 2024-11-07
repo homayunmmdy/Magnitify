d81c67db061736cd81483ef98030c452455da498
Homayoun <homayoun763@gmail.com>
2024-11-07 20:48:35 +0330

    add google analytics

--- src/app/(etc)/components/Dependencies.tsx
+++ src/app/(etc)/components/Dependencies.tsx
@@ -1,6 +1,7 @@
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import ScoialMediaSidebar from './ScoialMediaSidebar'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const Dependencies = () => {
     return (
@@ -8,6 +9,7 @@ const Dependencies = () => {
             <ScoialMediaSidebar />
             <Analytics />
             <SpeedInsights />
+            <GoogleAnalytics gaId="G-7ZRLWCZHL7" />
         </>
     )
 }
