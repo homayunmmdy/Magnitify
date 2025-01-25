01134f2966e25b1335b111591a7071f022debcac
Homayoun <129702378+homayunmmdy@users.noreply.github.com>
2025-01-25 13:54:46 +0330

    remove : date in postCard

--- src/components/sections/PostCard.tsx
+++ src/components/sections/PostCard.tsx
@@ -14,13 +14,7 @@ const PostCard = ({ post }: { post: PostsCashType }) => {
         href={`/posts/${post._id}`}
         className="md:hover:scale-102 group flex flex-wrap-reverse py-10 transition-transform duration-500 ease-in-out md:flex-nowrap"
       >
-        <div className="w-full px-5 pt-5 md:w-[20%]">
-          <FormatTime
-            timestamp={post.createdAt}
-            options={options as Intl.DateTimeFormatOptions}
-          />
-        </div>
-        <section className="w-full md:w-[80%]">
+        <section className="w-full">
           <h2 className="font-2xl mb-3 font-bold group-hover:text-amber-600">{post.title.slice(0, 120)}</h2>
           <p>{post.description.slice(0, 300)}</p>
         </section>
