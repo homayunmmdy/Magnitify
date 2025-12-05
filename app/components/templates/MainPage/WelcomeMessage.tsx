d47b5b53c4d384f4a6951521f49ce27957d7afdc
Homayoun <homayoun763@gmail.com>
2025-12-05 23:43:49 +0330

    implement : custom greeting for fa

--- app/components/templates/MainPage/WelcomeMessage.tsx
+++ app/components/templates/MainPage/WelcomeMessage.tsx
@@ -1,5 +1,6 @@
 import { useTranslations } from "next-intl";
 import { setRequestLocale } from "next-intl/server";
+import Image from "next/image";
 import Link from "next/link";
 import { use } from "react";
 
@@ -13,16 +14,24 @@ const WelcomeMessage = ({
   // Enable static rendering for this page
   setRequestLocale(locale);
 
+
   const t = useTranslations("main");
   return (
     <div className="flex-center flex-col p-7 mt-4 bg-[#f9f9f9] border border-[#ddd]">
-      <h2 className="font-[22.68px]">
-        {t("welcome_to")}{" "}
-        <Link className="link" title={t("name")} href="/">
-          {t("name")}
+      {locale === "fa" ? (
+        <Link title="به افغان پدیا خوش آمدید" href="/">
+          <Image className="pt-1.5 pb-3.5" src="/images/fa_greeting.png" width={250} height={96} alt="" />
         </Link>
-      </h2>
-      <p className="text-[13.3px]">{t('description')}</p>
+      ) : (
+        <h2 className="font-[22.68px]">
+          {t("welcome_to")}{" "}
+          <Link className="link" title={t("name")} href="/">
+            {t("name")}
+          </Link>
+        </h2>
+      )}
+
+      <p className="text-[13.3px]">{t("description")}</p>
     </div>
   );
 };
