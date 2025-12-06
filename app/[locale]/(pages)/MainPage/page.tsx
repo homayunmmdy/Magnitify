0a02c9f27e94221d33e217ceb29e1f2724f4ca01
Homayoun <homayoun763@gmail.com>
2025-12-06 19:13:24 +0330

    implement : Sidebar

--- app/[locale]/(pages)/MainPage/page.tsx
+++ app/[locale]/(pages)/MainPage/page.tsx
@@ -1,12 +1,18 @@
 import Header from "@/app/components/Header";
+import Sidebar from "@/app/components/templates/MainPage/Sidebar";
 import WelcomeMessage from "@/app/components/templates/MainPage/WelcomeMessage";
 
 const MainPage = ({ params }: { params: Promise<{ locale: string }> }) => {
   return (
     <>
-      <Header params={params}/>
-      <div className="px-4">
-        <WelcomeMessage params={params} />
+      <Header params={params} />
+      <div className="flex px-4 gap-6">
+        <div className="w-full md:w-5/6">
+          <WelcomeMessage params={params} />
+        </div>
+        <div className="hidden md:block w-1/6">
+          <Sidebar />
+        </div>
       </div>
     </>
   );
---
+++ app/components/templates/MainPage/Sidebar.tsx
@@ -0,0 +1,9 @@
+const Sidebar = () => {
+  return (
+    <div className="w-full px-4">
+      <b className="pb-1.5 block mb-1.5 border-b w-full border-[#eaecf0]">Appearance</b>
+    </div>
+  );
+};
+
+export default Sidebar;
