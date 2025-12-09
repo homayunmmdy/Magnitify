a5575ed219485d0b6b84640a2966beaa05bd2bdc
Homayoun <homayoun763@gmail.com>
2025-12-09 20:41:46 +0330

    fix : typo mistake in api/posts

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -3,9 +3,9 @@ import { PostModel } from "@/app/models";
 import { PostsCashType } from "@/app/types/DataTypes";
 import RequestHandler from "@/app/util/RequestHandler";
 
-export async function Get(req: Request): Promise<Response> {
+export async function GET(req: Request): Promise<Response> {
   const handler = new RequestHandler<PostsCashType>(PostModel, PostsCash);
-  return handler.Post(req, "Post Created successfully");
+  return handler.GetAll();
 }
 
 export async function POST(req: Request): Promise<Response> {
