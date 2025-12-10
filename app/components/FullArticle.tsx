362d9bdb6ac1b4261163d6752897b4c0c4ac086c
Homayoun <homayoun763@gmail.com>
2025-12-10 22:52:43 +0330

    support : html in full article

--- app/components/FullArticle.tsx
+++ app/components/FullArticle.tsx
@@ -1,4 +1,5 @@
 "use client";
+import DOMPurify from "isomorphic-dompurify";
 import Image from "next/image";
 import useFetch from "../hooks/useFetch";
 import { PostsCashType } from "../types/DataTypes";
@@ -6,17 +7,28 @@ import ContentWrapper from "./ContentWrapper";
 
 const FullPost = () => {
   const { data } = useFetch("article", "/api/posts");
+
   return (
     <ContentWrapper title="از میان خبرها">
-      {data?.map((post: PostsCashType) => ( 
-        <div key={post._id}>
-          <Image className="w-1/3 m-2.5 mr-4 mb-2 h-auto float-left" src={post.imgurl} width={124} height={124} alt="" />
-          <p>
-            <b>{post.title}</b>
-            {post.body}
-          </p>
-        </div>
-      ))}
+      {data?.map((post: PostsCashType) => {
+        const PostBody = DOMPurify.sanitize(post.body);
+        return (
+          <div key={post._id}>
+            <Image
+              className="w-1/3 m-2.5 mr-4 mb-2 h-auto float-left"
+              src={post.imgurl}
+              width={124}
+              height={124}
+              alt=""
+            />
+            <p>
+              <b>{post.title}</b>
+
+              <div dangerouslySetInnerHTML={{ __html: PostBody }} />
+            </p>
+          </div>
+        );
+      })}
     </ContentWrapper>
   );
 };
