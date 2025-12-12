30bfc2401abc8a5de7d09640b98c407f715c36af
Homayoun <homayoun763@gmail.com>
2025-12-12 23:49:46 +0330

    load : random article in the middle of the news section

--- app/components/FullArticle.tsx
+++ app/components/FullArticle.tsx
@@ -1,34 +1,49 @@
 "use client";
 import DOMPurify from "isomorphic-dompurify";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import useFetch from "../hooks/useFetch";
 import { PostsCashType } from "../types/DataTypes";
 import ContentWrapper from "./ContentWrapper";
 
 const FullPost = () => {
   const { data } = useFetch("article", "/api/posts");
+  const [randomPost, setRandomPost] = useState<PostsCashType | null>(null);
+
+  useEffect(() => {
+    if (data && data.length > 0) {
+      // Generate random index based on data length
+      const randomIndex = Math.floor(Math.random() * data.length);
+      // Select the random post
+      setRandomPost(data[randomIndex]);
+    }
+  }, [data]);
+
+  if (!randomPost) {
+    return (
+      <ContentWrapper title="از میان خبرها">
+        <p>در حال بارگذاری...</p>
+      </ContentWrapper>
+    );
+  }
+
+  const PostBody = DOMPurify.sanitize(randomPost.body);
 
   return (
     <ContentWrapper title="از میان خبرها">
-      {data?.map((post: PostsCashType) => {
-        const PostBody = DOMPurify.sanitize(post.body);
-        return (
-          <div key={post._id}>
-            <Image
-              className="w-1/3 m-2.5 mr-4 mb-2 h-auto float-left"
-              src={post.imgurl}
-              width={124}
-              height={124}
-              alt=""
-            />
-            <p>
-              <b>{post.title}</b>
-
-              <div dangerouslySetInnerHTML={{ __html: PostBody }} />
-            </p>
-          </div>
-        );
-      })}
+      <div key={randomPost._id}>
+        <Image
+          className="w-1/3 m-2.5 mr-4 mb-2 h-auto float-left"
+          src={randomPost.imgurl}
+          width={124}
+          height={124}
+          alt={randomPost.title}
+        />
+        <p>
+          <b>{randomPost.title}</b>
+          <div dangerouslySetInnerHTML={{ __html: PostBody }} />
+        </p>
+      </div>
     </ContentWrapper>
   );
 };
