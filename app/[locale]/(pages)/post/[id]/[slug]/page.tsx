4d3fbaa41cd3e6d7aa8bd390e0bc7e929880ad1e
Homayoun <homayoun763@gmail.com>
2025-12-19 00:04:23 +0330

    implement : body of post

--- app/[locale]/(pages)/post/[id]/[slug]/page.tsx
+++ app/[locale]/(pages)/post/[id]/[slug]/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 import useSinglePost from "@/app/hooks/useSinglePost";
+import DOMPurify from "isomorphic-dompurify";
 import { useParams, useRouter } from "next/navigation";
 import { useEffect } from "react";
+import '../../../../tiptap.css'
 
 const slugify = (title: string) =>
   title
@@ -24,9 +26,20 @@ const PostPage = () => {
     }
   }, [post, slug, router]);
 
-  return <div>
-    <h1 className="text-2xl font-libertine border-b border-[#a2a9b1] pb-1">{post.title}</h1>
-  </div>;
+  const PostBody = DOMPurify.sanitize(post?.body);
+
+  return (
+    <div>
+      <h1 className="text-2xl font-libertine border-b border-[#a2a9b1] pb-1">
+        {post?.title}
+      </h1>
+      <div
+        className="prose mb-3 max-w-none text-base leading-9"
+        id="tiptap-style"
+        dangerouslySetInnerHTML={{ __html: PostBody }}
+      />
+    </div>
+  );
 };
 
 export default PostPage;
